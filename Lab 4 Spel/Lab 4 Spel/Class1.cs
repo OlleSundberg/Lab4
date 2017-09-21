2c397b86f72a74273ed5f81e39e91a244a8e91d1
Olle Sundberg <olle.sundbergg@gmail.com>
2017-09-21 12:04:07 +0200

    Add Class1.cs

---
+++ Lab 4 Spel/Lab 4 Spel/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Spel
+{
+    class Class1
+    {
+        int x = 3;
+    }
+}
