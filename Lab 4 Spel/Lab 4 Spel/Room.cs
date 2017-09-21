f738ab87d136d2d8cbc999960952d4ff27817cc4
Olle Sundberg <olle.sundbergg@gmail.com>
2017-09-21 14:21:17 +0200

    Change Room.cs

--- Lab 4 Spel/Lab 4 Spel/Room.cs
+++ Lab 4 Spel/Lab 4 Spel/Room.cs
@@ -6,7 +6,52 @@ using System.Threading.Tasks;
 
 namespace Lab_4_Spel
 {
-    class Room
+    interface IMapIcon
     {
+        char mapIcon { get; set; }
+    }
+    abstract class Room : IMapIcon
+    {
+        public int x, y;
+        public char mapIcon { get; set; }
+        public Program.RoomType type;
+
+
+        public Room(int x, int y, char mapIcon, Program.RoomType type)
+        {
+            this.x = x;
+            this.y = y;
+            this.mapIcon = mapIcon;
+            this.type = type;
+        }
+    }
+    class MonsterRoom : Room
+    {
+        public MonsterRoom(int x, int y) : base(x, y, 'M', Program.RoomType.Monster) { }
+        int monsterHP = 100;
+    }
+    class DoorRoom : Room
+    {
+        public DoorRoom(int x, int y, string doorColor) : base(x, y, 'D', Program.RoomType.Door)
+        {
+            this.doorColor = doorColor;
+        }
+        string doorColor;
+    }
+    class KeyRoom : Room
+    {
+        public KeyRoom(int x, int y, string keyColor) : base(x, y, 'n', Program.RoomType.Key)
+        {
+            this.keyColor = keyColor;
+        }
+        string keyColor;
+    }
+    class Wall : Room
+    {
+        public Wall(int x, int y) : base(x, y, '#', Program.RoomType.Wall) { }
+    }
+    class Empty : Room
+    {
+        public Empty(int x, int y) : base(x, y, '.', Program.RoomType.Empty) { }
     }
 }
