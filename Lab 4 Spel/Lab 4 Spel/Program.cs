bf048946102a288f89208c2e12e7de5b3c1d740c
Olle Sundberg <olle.sundbergg@gmail.com>
2017-09-22 12:23:55 +0200

    (Change from ReadKey().KeyChar to .Key)

--- Lab 4 Spel/Lab 4 Spel/Program.cs
+++ Lab 4 Spel/Lab 4 Spel/Program.cs
@@ -155,10 +155,7 @@ namespace Lab_4_Spel
                 Console.WriteLine("");
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("Turns: " + turns);
-
-
-                // Beskriv rummet (baserat på enum?):
-
+               
                 switch (Console.ReadKey(true).Key)
                 {
                     case ConsoleKey.W:
