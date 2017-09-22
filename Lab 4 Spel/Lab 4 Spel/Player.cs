62849519e75a30639d3fb8f17c142a5b8ac94da2
Olle Sundberg <olle.sundbergg@gmail.com>
2017-09-22 12:09:39 +0200

    Add hardcore vision mode. Turn Player.HP into Player.HitPoints

--- Lab 4 Spel/Lab 4 Spel/Player.cs
+++ Lab 4 Spel/Lab 4 Spel/Player.cs
@@ -10,15 +10,21 @@ namespace Lab_4_Spel
     {
         public int X;
         public int Y;
-        public int HP = 100;
+        private int hitPoints = 100;
+        public int HP
+        {
+            get { return hitPoints; }
+            set { hitPoints = value; }
+        }
         public bool wasHurt = false;
         public bool wasSpiked = false;
         public bool foundTreasure = false;
+        public char playerIcon = '@';
 
         public Player(int x, int y)
         {
             X = x;
-            Y = y;            
+            Y = y;
         }
         public void check(Room[,] map)
         {
@@ -26,6 +32,21 @@ namespace Lab_4_Spel
             map[X + 1, Y].visible = true;
             map[X, Y + 1].visible = true;
             map[X, Y - 1].visible = true;
+        }
+        public void fow(Room[,] map, int w, int h)
+        {
+            for (int x = 1; x < w - 1; x++)
+            {
+                for (int y = 1; y < h - 1; y++)
+                {
+                    map[x, y].visible = false;
+                }
+            }
+            map[X - 1, Y - 1].visible = true;
+            map[X + 1, Y - 1].visible = true;
+            map[X - 1, Y + 1].visible = true;
+            map[X + 1, Y + 1].visible = true;
+            check(map);
         }
     }
 }
